f98e4628301fb9bb53313d6e2be0205205093f41
Tabash <u9215122@bsmch.net>
2026-03-21 14:49:38 +0200

    feat: aff words to bank

---
+++ utils/verifyWords/deduplicate_heb.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = "../../constants/wordBanks/heb.json";
+const data = JSON.parse(fs.readFileSync(path, "utf8"));
+const originalCount = data.words.length;
+const uniqueWords = [...new Set(data.words)];
+const newCount = uniqueWords.length;
+data.words = uniqueWords;
+fs.writeFileSync(path, JSON.stringify(data, null, 2));
+console.log(
+  "Original: " +
+    originalCount +
+    ", Unique: " +
+    newCount +
+    ", Removed: " +
+    (originalCount - newCount),
+);
