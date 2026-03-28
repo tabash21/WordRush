9f7da823c6dd4cbb7b78ecf2d780a1e031906279
Tal <talb2111@gmail.com>
2026-03-28 14:55:35 +0300

    fix: dark mode

--- hooks/use-color-scheme.ts
+++ hooks/use-color-scheme.ts
@@ -1,1 +1,6 @@
-export { useColorScheme } from 'react-native';
+// export { useColorScheme } from 'react-native';
+// todo: remove this when we have a real color scheme
+
+const useColorScheme = () => "dark";
+
+export { useColorScheme };
